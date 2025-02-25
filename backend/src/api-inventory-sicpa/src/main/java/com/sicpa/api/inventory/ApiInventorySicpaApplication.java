6a5673fa9209485fd8d0e90fb7fb25466e77dfcc
Rodrigo Lago <rplago@gmail.com>
2025-02-24 22:53:22 -0300

    basic project with spring boot

---
+++ backend/src/api-inventory-sicpa/src/main/java/com/sicpa/api/inventory/ApiInventorySicpaApplication.java
@@ -0,0 +1,13 @@
+package com.sicpa.api.inventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiInventorySicpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiInventorySicpaApplication.class, args);
+	}
+
+}
---
+++ backend/src/api-inventory-sicpa/src/test/java/com/sicpa/api/inventory/ApiInventorySicpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sicpa.api.inventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiInventorySicpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
